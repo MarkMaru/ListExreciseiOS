d67933348e8b803ce97b1ea396822eddcbf3da88
Mark Marusic <mark.marusic@gmail.com>
2024-02-27 17:07:15 +0100

    Initial Commit

---
+++ ListExercise/ListExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListExerciseApp.swift
+//  ListExercise
+//
+//  Created by GCCISAdmin on 11/8/22.
+//
+
+import SwiftUI
+
+@main
+struct ListExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
